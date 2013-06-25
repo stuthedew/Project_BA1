ef5e50808776d4a9310e456005ce78666fccdd62
stuthedew <stuart.feichtinger@gmail.com>
2013-06-25 14:04:32 -0600

    Created library to handle GPS data

---
+++ modules/data.h
@@ -0,0 +1,18 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef data_H_
+#define data_H_
+#include "Arduino.h"
+
+//add your includes for the project data here
+
+
+
+
+
+//Do not add code below this line
+#endif /* data_H_ */
