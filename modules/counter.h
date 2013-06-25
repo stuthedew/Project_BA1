32f6040c43bac9f09daecbaaee044eee62984911
stuthedew <stuart.feichtinger@gmail.com>
2013-06-25 13:58:22 -0600

    Created library to run impulse counter

---
+++ modules/counter.h
@@ -0,0 +1,18 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef counter_H_
+#define counter_H_
+#include "Arduino.h"
+
+//add your includes for the project counter here
+
+
+
+
+
+//Do not add code below this line
+#endif /* counter_H_ */
