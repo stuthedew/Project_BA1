18c3e777b1870413fcadd120f247f9ed0c55d4ca
stuthedew <stuart.feichtinger@gmail.com>
2013-06-25 14:02:36 -0600

    Created library to interface with led button

---
+++ modules/button.h
@@ -0,0 +1,18 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef button_H_
+#define button_H_
+#include "Arduino.h"
+
+//add your includes for the project button here
+
+
+
+
+
+//Do not add code below this line
+#endif /* button_H_ */
