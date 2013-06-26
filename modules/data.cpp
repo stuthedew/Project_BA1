d361207e6ae5053b75016a0d94a1526b52807c29
stuthedew <stuart.feichtinger@gmail.com>
2013-06-26 16:18:40 -0600

    Added secret location values

--- modules/data.cpp
+++ modules/data.cpp
@@ -1,6 +1,9 @@
 
 #include <data.h>
 #include <Flash.h>
+
+/**********************************SECRET LOCATION*********************************/
+/*		 +	-
  *------------
  * lat | N	S
  * lon | E  W
@@ -9,6 +12,7 @@
 // lat, lon
 FLASH_ARRAY(float, Secretlocation, 43.691708, -114.35051);
 
+/**********************************SECRET LOCATION*********************************/
 FLASH_ARRAY(float, calcConsts, 0.017453292519943295769236907684886, 6372797.560856, 0.000621371);
 	//calcConsts[0] == degrees to radian conversion constant
     double latitudeArc  = (from.lat - to.lat) * calcConsts[0];
