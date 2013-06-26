5b188df588efe45b6d0028fc4a9a7cd29a30713e
stuthedew <stuart.feichtinger@gmail.com>
2013-06-26 17:21:52 -0600

    Relocated libraries and fixed linkages

--- Project_BA.ino
+++ Project_BA.ino
@@ -1,6 +1,15 @@
+#include <cache.h>
+#include <Flash.h>
+//#include <modules/button.h>
+//#include <modules/counter.h>
+#include <data.h>
+//#include <modules/lock.h>
+//#include <modules/meter.h>
+//#include <modules/GPS.h>
 
-#include "cache.h"
 
+
+Data locationData();
 
 void setup(){
   Serial.begin(57600);
--- modules/counter.cpp
+++
@@ -1,2 +0,0 @@
-
-#include <counter.h>
